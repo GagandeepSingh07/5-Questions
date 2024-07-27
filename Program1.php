2e4fa1972e02376a35e939bef91820283e8c8c3a
GagandeepSingh07 <singhgagan40951@gmail.com>
2024-07-27 09:43:46 +0530

    Add php & css file

---
+++ Program1.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Question Paper</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    echo '
+    <div class="title-container">
+        <h1 class="title">Question Paper</h1>
+    </div>';
+
+    echo '
+<div class="parent-container">
+    <div class="sec-a-container">
+        <ul>
+            <li><p>What is your favorite food?</p>
+                <ol>
+                    <li>Why do you like it?</li>
+                    <li>How often do you eat it?</li>
+                    <li>Is there a specific place you like to get it from?</li>
+                </ol>
+            </li>
+        </ul>
+    </div>';
+
+    echo '
+    <div class="sec-b-container">
+        <ul>
+            <li><p>What is your favorite book?</p>
+                <ol>
+                    <li>Who is the author?</li>
+                    <li>What is the main plot or theme?</li>
+                    <li>Why do you recommend it to others?</li>
+                </ol>
+            </li>
+        </ul>
+    </div>';
+
+    echo '
+    <div class="sec-c-container">
+        <ul>
+            <li><p>What is your favorite hobby?</p>
+                <ol>
+                    <li>How did you get started with it?</li>
+                    <li>How often do you practice it?</li>
+                    <li>What do you enjoy most about it?</li>
+                </ol>
+            </li>
+        </ul>
+    </div>';
+
+    echo '
+    <div class="sec-d-container">
+        <ul>
+            <li><p>What is your favorite movie?</p>
+                <ol>
+                    <li>Who is the director?</li>
+                    <li>Who is the director?</li>
+                    <li>Why do you like it?</li>
+                </ol>
+            </li>
+        </ul>
+    </div>';
+
+    echo '
+    <div class="sec-e-container">
+        <ul>
+            <li><p>What is your dream travel destination?</p>
+                <ol>
+                    <li>What attracts you to this place?</li>
+                    <li>How long would you like to stay there?</li>
+                    <li>What activities would you like to do while there?</li>
+                </ol>
+            </li>
+        </ul>
+    </div>
+</div>';
+    ?>
+</body>
+</html>
